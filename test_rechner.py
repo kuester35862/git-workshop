5ba9e150c6a8341f4299b057ed2b4bd3cf468de7
Marcel Reichert <marcel.reichert@kuester-group.com>
2026-05-06 13:33:41 +0200

    Fehler korrigiert

--- test_rechner.py
+++ test_rechner.py
@@ -2,8 +2,8 @@ from rechner import addiere, subtrahiere
 
 
 def test_addiere():
-    assert addiere(2, 3) == 55
+    assert addiere(2, 3) == 5
 
 
 def test_subtrahiere():
-    assert subtrahiere(5, 3) == 22
+    assert subtrahiere(5, 3) == 2
