88e77f46e6b0f6143fb807eeab18aeb361ae1f5a
Marcel Reichert <marcel.reichert@kuester-group.com>
2026-05-06 13:30:57 +0200

    Fehler eingebaut

--- test_rechner.py
+++ test_rechner.py
@@ -2,8 +2,8 @@ from rechner import addiere, subtrahiere
 
 
 def test_addiere():
-    assert addiere(2, 3) == 5
+    assert addiere(2, 3) == 55
 
 
 def test_subtrahiere():
-    assert subtrahiere(5, 3) == 2
+    assert subtrahiere(5, 3) == 22
